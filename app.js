1b2bd8d12dc53dbc1f8590beb53035dc348e3c48
FATMAKAMSHAD90 <fatmakamshad90@gmail.com>
2022-01-30 12:47:45 +0300

    done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const products = require("./products");
+let products = require("./products");
 const res = require("./products");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("hello");
@@ -11,4 +12,27 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
-app.listen(8000, () => console.log("this app is running on 8000"));
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  const id = products.length + 1;
+  const newProduct = { ...req.body, id: id };
+  products.push(newProduct);
+  res.status(201);
+  res.json(newProduct);
+});
+
+app.delete("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  const foundProduct = products.find((product) => product.id === +productId);
+  if (foundProduct) {
+    const newArray = products.filter((product) => product.id !== +productId);
+    products = newArray;
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.listen(8000, () => {
+  console.log("this app is running on 8000");
+});
