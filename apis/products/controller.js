68d2fcf4428f56a32c68a02a9d07906492ff441c
FATMAKAMSHAD90 <fatmakamshad90@gmail.com>
2022-02-02 10:18:11 +0300

    middleware

--- apis/products/controller.js
+++ apis/products/controller.js
@@ -1,47 +1,47 @@
 let products = require("./products");
 const Product = require("./db/models/Product");
 
-exports.fetchProducts = async (req, res) => {
+exports.fetchProducts = async (req, res, next) => {
   try {
     const productArray = await Product.find({}).select(
       "name description price color"
     );
     res.json(productArray);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.getDetail = (req, res) => {
+exports.getDetail = (req, res, next) => {
   try {
     const { productId } = req.params;
     const productOne = products.find((e) => e.id === +productId);
     res.status(200).jsom(productOne);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.createProducts = async (req, res) => {
+exports.createProducts = async (req, res, next) => {
   try {
     const newProduct = await Product.create(req.body);
     res.status(201).json(newProduct);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.deleteProducts = async (req, res) => {
+exports.deleteProducts = async (req, res, next) => {
   const { productId } = req.params;
   try {
     const foundProduct = await Product.findByIdAndDelete({ _id: productId });
     if (foundProduct) res.status(204).end();
-    else res.status(404).end();
+    else next({ status: 404, message: "Product not found" });
   } catch (error) {
-    res.status(500).jsom({ message: error.message });
+    next(error);
   }
 };
-exports.updateProduct = async (req, res) => {
+exports.updateProduct = async (req, res, next) => {
   try {
     const { productId } = req.params;
     const product = await Product.findByIdAndUpdate(
@@ -50,8 +50,8 @@ exports.updateProduct = async (req, res) => {
       { new: true, runValidators: true }
     );
     if (product) res.json(product);
-    else res.status(404).json({ message: "not found" });
+    else next({ status: 404, message: "Product not found" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const productsRoutes = require("./apis/products/routes");
 const connectDB = require("./apis/products/db/database");
+const PORT = 8000;
 const app = express();
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.protocol}://${req.get("host")}${req.path}`);
+  next();
+});
+
 app.use("api/products", productsRoutes);
+//Midlleware
+app.use((req, res) => {
+  res.status(404).json({ message: "path not found" });
+});
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal server issues" });
+});
 
 connectDB();
 app.listen(8000, () => {
